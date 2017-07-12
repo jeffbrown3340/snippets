7d8dd3211c133116bf75d361a0139ca15d90fc94
Jeffrey Brown <jeff@jeffbrown.us>
2017-07-12 01:37:56 -0400

    create is working

--- snip1/Controllers/HomeController.cs
+++ snip1/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MongoDB.Driver;
 
 namespace snip1.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
@@ -28,9 +30,19 @@ namespace snip1.Controllers
         }
         public ActionResult Create()
         {
+
             ViewBag.Message = "This is our CREATE page.";
 
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Snips snip)
+        {
+            var mongoClient = new MongoClient();
+            var db = mongoClient.GetDatabase("snippets");
+            var snippetCollection = db.GetCollection<Snips>("snippets");
+            snippetCollection.InsertOne(snip);
+            return Redirect("/Home/Create");
         }
         public ActionResult Read()
         {
---
+++ snip1/Models/Snip.cs
@@ -0,0 +1,4 @@
+﻿    public class Snips
+    {
+    public string aSnip { get; set; }
+    }
